a149b22b08a92ab7add5598d19446a043101bbbc
mbasaran77 <mbasaran77@gmail.com>
2017-05-10 18:01:37 +0300

    datetime_00

--- d_base_mdl.py
+++ d_base_mdl.py
@@ -1,9 +1,10 @@
 import sqlite3
-
+import datetime
 class d_base():
     def __init__(self):
         self.conn = sqlite3.connect('vpdata.db')
         self.c = self.conn.cursor()
+        self.c.execute('pragma foreign_keys=ON')
 
 
     def create_table_rec_data(self,table_name,data_column_name):
@@ -25,14 +26,17 @@ class d_base():
         rec_prg_id = 123
         rec_date = 1
         rec_time = 0
-        rec_data = 4.5
+        rec_data = datetime.date.today()
         data_t =(rec_bact_id,rec_prg_id,rec_date,rec_time,rec_data)
         self.c.execute('INSERT INTO {tn}(batch_id,prg_id,date_stamp,time_stamp,{dn}) VALUES (?,?,?,?,?)'.format(tn=table_name,dn=data_column_name),data_t)
         self.conn.commit()
         print("data inserted")
 
-    def read_db(self):
-        pass
+    def view_data(self,table_name):
+        self.c.execute("SELECT * FROM {tn}".format(tn=table_name))
+        # cur.execute("SELECT * FROM yazar_ad")
+        rows = self.c.fetchall()
+        return rows
 
     def close_db(self):
         self.conn.close()
@@ -41,7 +45,12 @@ vt = d_base()
 
 vt.create_table_rec_data('sicaklik_table',"isi")
 # vt.delete_table('sicaklik_table')
-vt.insert_data('sicaklik_table','isi',5.5)
+#vt.insert_data('sicaklik_table','isi',5.5)
+
+a = vt.view_data('sicaklik_table')
+print(a)
+
+
 vt.close_db()
 
 
