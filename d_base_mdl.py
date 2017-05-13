7f4aa37bb307e724c23cf0fe3f6aa35fd72b3c51
mbasaran77 <mbasaran77@gmail.com>
2017-05-13 13:10:11 +0300

    date_time_001

--- d_base_mdl.py
+++ d_base_mdl.py
@@ -11,22 +11,33 @@ class d_base():
 
         try:
             self.c.execute('CREATE TABLE IF NOT EXISTS {tn}(sicaklik_id INTEGER PRIMARY KEY AUTOINCREMENT, batch_id INTEGER,'
-                           'prg_id INTEGER, date_stamp NUMERIC, time_stamp NUMERIC, {dn} REAL)'.format(tn=table_name,dn=data_column_name))
+                           'prg_id INTEGER, date_stamp STRING, time_stamp STRING, {dn} REAL)'.format(tn=table_name,dn=data_column_name))
             print("db created")
         except sqlite3.OperationalError:
             print("err in table create")
+
+    def create_table_prg(self,table_name):
+        try:
+            self.c.execute('CREATE TABLE IF NOT EXISTS {tn}(id INTEGER PRIMARY KEY AUTOINCREMENT, prg_name STRING)'.format(tn=table_name))
+            print("prg table created")
+        except sqlite3.OperationalError:
+            print("err in prg table create")
     def delete_table(self,table_name):
         try:
             self.c.execute('DROP TABLE  {tn}'.format(tn=table_name))
+            self.conn.commit()
             print("db deleted")
         except sqlite3.OperationalError:
             print("err in table delete")
     def insert_data(self,table_name,data_column_name,data_insert):
+        an = datetime.datetime.now()
+        tarih = datetime.datetime.strftime(an, '%x')
+        saat = datetime.datetime.strftime(an, '%X')
         rec_bact_id = 112
         rec_prg_id = 123
-        rec_date = 1
-        rec_time = 0
-        rec_data = datetime.date.today()
+        rec_date = tarih
+        rec_time = saat
+        rec_data = data_insert
         data_t =(rec_bact_id,rec_prg_id,rec_date,rec_time,rec_data)
         self.c.execute('INSERT INTO {tn}(batch_id,prg_id,date_stamp,time_stamp,{dn}) VALUES (?,?,?,?,?)'.format(tn=table_name,dn=data_column_name),data_t)
         self.conn.commit()
@@ -38,6 +49,7 @@ class d_base():
         rows = self.c.fetchall()
         return rows
 
+
     def close_db(self):
         self.conn.close()
 
@@ -45,7 +57,8 @@ vt = d_base()
 
 vt.create_table_rec_data('sicaklik_table',"isi")
 # vt.delete_table('sicaklik_table')
-#vt.insert_data('sicaklik_table','isi',5.5)
+vt.create_table_prg('program_table')
+vt.insert_data('sicaklik_table','isi',5.5)
 
 a = vt.view_data('sicaklik_table')
 print(a)
